f9ae4835643d12bd9609422c203bdf3fc5abd04f
sujijava <59837559+sujijava@users.noreply.github.com>
2020-11-10 20:31:09 -0500

    ipdata used

--- src/components/Geolocation/Geolocation.js
+++ src/components/Geolocation/Geolocation.js
@@ -1,48 +1,59 @@
 import React, {Component} from 'react';
-
-
+import axios from 'axios';
 
 class Geolocation extends Component {
 
   state = {
-    
-  city: null,
-  country: null,
   availableArea: false,
-  inCanada: false}; 
+  inCanada: false,
+  geoResponse: []
+}; 
 
   componentDidMount(){
-    fetch('http://api.ipstack.com/check?access_key=dd3d2ab8fa46c73f3963a828a513d04e')
-    .then(res => res.json())
-    .then((result)=>{
-      this.setState({city: result.city});
-      this.setState({country: result.country_name})
-      console.log(this.state.city);
+
+
+    axios.get('https://api.ipdata.co?api-key=e743783864e208f0fb2b0a3b6f493eaf92de6df082dbdcd101edcc9d')
+    .then(response => {
+      this.setState({geoResponse: response.data});
     })
     .then(()=>{
       this.gtaChecker()
-      this.canadaChecker()});
+      this.canadaChecker()
+    });
   }
 
+
   gtaChecker(){
-    if(this.state.city.toLowerCase()==="oakville"){
+    if(this.state.geoResponse.city.toLowerCase()==="oakville"){
       this.setState({availableArea: true});
     }
 
-    if(this.state.city.toLowerCase()==="vancouver"){
+    if(this.state.geoResponse.city.toLowerCase()==="toronto"){
+      this.setState({availableArea: true});
+    }
+
+    if(this.state.geoResponse.city.toLowerCase()==="waterloo"){
+      this.setState({availableArea: true});
+    }
+
+    if(this.state.geoResponse.city.toLowerCase()==="ottawa"){
+      this.setState({availableArea: true});
+    }
+    
+    if(this.state.geoResponse.city.toLowerCase()==="vancouver"){
      this.setState({availableArea: true}); 
     }
   }
 
   canadaChecker(){
-    if(this.state.country.toLowerCase()==="canada"){
+    if(this.state.geoResponse.country_name.toLowerCase()==="canada"){
       this.setState({inCanada: true})
     }
   }
 
   render(){
 
-    let deliveryMessage = `Your are in ${this.state.city}, ${this.state.country}`;
+    let deliveryMessage = `Your are in ${this.state.geoResponse.city}, ${this.state.geoResponse.country_name}`;
 
     let availableCountryMessage = null;
 
@@ -57,8 +68,7 @@ class Geolocation extends Component {
       {availableCountryMessage}
     </div>
     )
-  }
 
 }
-
+}
 export default Geolocation;
